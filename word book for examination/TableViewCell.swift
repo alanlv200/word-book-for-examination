dab69020e1a2fa372dc58f44b5ab13a370466515
alanlv200 <vistlv200@gmail.com>
2016-02-26 11:41:31 +0900

    first commit

---
+++ word book for examination/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  word book for examination
+//
+//  Created by 平山大翼 on 2016/02/20.
+//  Copyright © 2016年 平山大翼. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ word book for examination/countdownViewController.swift
@@ -0,0 +1,35 @@
+//
+//  countdownViewController.swift
+//  word book for examination
+//
+//  Created by 平山大翼 on 2016/02/25.
+//  Copyright © 2016年 平山大翼. All rights reserved.
+//
+
+import UIKit
+
+class countdownViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
